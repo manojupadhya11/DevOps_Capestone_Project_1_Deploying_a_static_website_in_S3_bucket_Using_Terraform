84a7da931a2644af29dbdedb32573b488492da8e
manojupadhya11 <manojupadhya11@gmail.com>
2024-05-27 08:56:28 +0530

    S3 bucket access policy setup completed

--- main.tf
+++ main.tf
@@ -6,6 +6,30 @@ resource "aws_s3_bucket" "ManojResto" {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.ManojResto.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.ManojResto.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.ManojResto.id
+  acl    = "public-read"
+}
+
